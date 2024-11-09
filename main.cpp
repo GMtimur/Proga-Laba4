35de4042359dc3aa95d8a5072f6ca0eda8ffdbc0
GMtimur <gmtimurtwitch@gmail.com>
2024-11-09 14:24:01 +0300

    Main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 
 using namespace std;
+
 const int MAX_N = 100;
 const int MAX_M = 100;
 
@@ -24,7 +25,7 @@ void printMatrix(double matrix[MAX_N][MAX_M], int rows, int cols) {
 
 bool multiplyMatrices(double A[MAX_N][MAX_M], int A_rows, int A_cols, 
                       double B[MAX_N][MAX_M], int B_rows, int B_cols, 
-                      double result[MAX_N][MAX_M]) {
+                      double result[MAX_N][MAX_M], int &result_rows, int &result_cols) {
     if (A_cols != B_rows) {
         return false;
     }
@@ -36,11 +37,13 @@ bool multiplyMatrices(double A[MAX_N][MAX_M], int A_rows, int A_cols,
             }
         }
     }
+    result_rows = A_rows;
+    result_cols = B_cols;
     return true;
 }
 
-bool powerMatrix(double A[MAX_N][MAX_M], int N, int M, double result[MAX_N][MAX_M]) {
-    if (N != M) {
+bool powerMatrix(double A[MAX_N][MAX_M], int N, int exp, double result[MAX_N][MAX_M]) {
+    if (N <= 0 || exp < 1) {
         return false;
     }
     for (int i = 0; i < N; ++i) {
@@ -49,8 +52,8 @@ bool powerMatrix(double A[MAX_N][MAX_M], int N, int M, double result[MAX_N][MAX_
         }
     }
     double temp[MAX_N][MAX_M];
-    for (int exp = 1; exp < N; ++exp) {
-        if (!multiplyMatrices(result, N, N, A, N, N, temp)) {
+    for (int i = 1; i < exp; ++i) {
+        if (!multiplyMatrices(result, N, N, A, N, N, temp, N, N)) {
             return false;
         }
         for (int i = 0; i < N; ++i) {
@@ -61,6 +64,7 @@ bool powerMatrix(double A[MAX_N][MAX_M], int N, int M, double result[MAX_N][MAX_
     }
     return true;
 }
+
 
 int main(int argc, char* argv[]) {
     setlocale(LC_CTYPE, "rus");
@@ -74,7 +78,6 @@ int main(int argc, char* argv[]) {
         int command;
         if (isHuman) cout << "Введите номер команды (1, 2, 3): ";
         cin >> command;
-
         if (command == 0) {
             break;
         } else if (command == 1) {
@@ -87,27 +90,30 @@ int main(int argc, char* argv[]) {
             double A2[MAX_N][MAX_M];
             inputMatrix(A2, M, K);
             double result[MAX_N][MAX_M];
-            if (!multiplyMatrices(A1, N, M, A2, M, K, result)) {
+            int result_rows, result_cols;
+            if (!multiplyMatrices(A1, N, M, A2, M, K, result, result_rows, result_cols)) {
                 cout << (isHuman ? "Матрицы нельзя перемножить!" : "NO") << endl;
             } else {
-                N = N;
+                N = result_rows;
+                M = result_cols;
                 for (int i = 0; i < N; ++i) {
-                    for (int j = 0; j < K; ++j) {
+                    for (int j = 0; j < M; ++j) {
                         A1[i][j] = result[i][j];
                     }
                 }
-                M = K;
             }
         } else if (command == 3) {
             if (N != M) {
                 cout << (isHuman ? "Матрицу нельзя возвести в степень!" : "NO") << endl;
                 continue;
             }
+
             int x;
             if (isHuman) cout << "Введите степень: ";
             cin >> x;
+
             double result[MAX_N][MAX_M];
-            if (!powerMatrix(A1, N, M, result)) {
+            if (!powerMatrix(A1, N, x, result)) {
                 cout << (isHuman ? "Матрицу нельзя возвести в степень!" : "NO") << endl;
             } else {
                 for (int i = 0; i < N; ++i) {
@@ -120,5 +126,6 @@ int main(int argc, char* argv[]) {
             cout << (isHuman ? "Некорректная команда. Попробуйте снова." : "NO") << endl;
         }
     }
+
     return 0;
 }
